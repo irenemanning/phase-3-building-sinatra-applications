bf8bf577e702050d8eb44e6b7e69cfd6ccfd679c
Irene <irenemanningm@gmail.com>
2022-10-02 21:41:40 -0500

    commit tree 6818aa674a0c06158adc746a05898df211c19ed6

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,10 @@
+ require 'sinatra'
+#  require_relative "./config/environment"
+
+class ApplicationController < Sinatra::Base
+
+    get '/' do
+      '<h2>Hello <em>World</em>!</h2>'
+    end
+  
+  end
